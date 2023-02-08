c73afc987b1dca1f54009c51eb9bf0022cecea1a
Yegor21 <egorossipik@mail.ru>
2023-02-09 01:56:33 +0300

    Основы Python (элементарные задачки)

---
+++ practice1.py
@@ -0,0 +1,17 @@
+print(type(7))
+print(type('hello'))
+print(type(str))
+print(type(7))
+print(type(9.99))
+
+num = -1.6
+int_round = round(num)
+print(int_round)
+
+
+num = 2.235566470338304765
+f_r = round(num, 5)
+print(f_r)
+
+spisok = [123, 'dfr', 'p812']
+print(spisok)
