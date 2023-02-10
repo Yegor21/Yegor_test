534e9a91d7ff591475d9cb2fd7087529b9614177
Yegor21 <egorossipik@mail.ru>
2023-02-10 23:09:13 +0300

    Основы Python (элементарные задачки)

--- practice1.py
+++ practice1.py
@@ -6,12 +6,14 @@ print(type(9.99))
 
 num = -1.6
 int_round = round(num)
-print(int_round)
+    print(int_round)
 
 
 num = 2.235566470338304765
 f_r = round(num, 5)
-print(f_r)
+    print(f_r)
 
+#3
 spisok = [123, 'dfr', 'p812']
-print(spisok)
+    print(spisok)
+
